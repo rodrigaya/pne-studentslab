be3158a376a4c01962744fb28eec9248a7efde91
rgaya <r.gaya.2023@alumnos.urjc.es>
2024-02-08 18:10:06 +0100

    S05

--- P00/e6.py
+++ P00/e6.py
@@ -1,4 +1,6 @@
 from Seq0 import seq_reverse, seq_read_fasta
 
-print(seq_read_fasta('../S04/Sequences/U5.fa'))
-print(seq_reverse(seq_read_fasta('../S04/Sequences/U5.fa'))[0:20])
+
+print('------| Exercise 6 |------' +
+      '\nFragement: ' + seq_read_fasta('../S04/Sequences/U5.fa')[0:20] +
+      '\nReverse: ' + seq_reverse(seq_read_fasta('../S04/Sequences/U5.fa'))[-20:])
