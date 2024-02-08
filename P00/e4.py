0981f23d228ce8dede1a0195f2d1bf1f84738182
rgaya <r.gaya.2023@alumnos.urjc.es>
2024-02-08 17:12:14 +0100

    S05

--- P00/e4.py
+++ P00/e4.py
@@ -1,18 +1,17 @@
 from Seq0 import seq_count_base, seq_read_fasta
 
-paths = ['../S04/Sequences/U5.fa', '../S04/Sequences/ADA.fa', '../S04/Sequences/FRAT1.fa',
-         '../S04/Sequences/FXN.fa']
+folder = '../S04/Sequences/'
+paths = ['U5.fa', 'ADA.fa', 'FRAT1.fa', 'FXN.fa']
 bases = ['A', 'C', 'T', 'G']
 lengths = []
 for n in paths:
     n_bs = []
     for m in bases:
-        n_b = seq_count_base(seq_read_fasta(n), m)
+        n_b = seq_count_base(seq_read_fasta(folder + n), m)
         n_bs.append(n_b)
     lengths.append(dict(zip(bases, n_bs)))
 
 data = dict(zip(paths, lengths))
-print(data)
 
 print('-----| Exercise 4 |------')
 for n in data:
--- P00/e5.py
+++ P00/e5.py
@@ -1,14 +1,13 @@
 from Seq0 import seq_count, seq_read_fasta
 
-
-paths = ['../S04/Sequences/U5.fa', '../S04/Sequences/ADA.fa', '../S04/Sequences/FRAT1.fa',
-         '../S04/Sequences/FXN.fa']
+folder = '../S04/Sequences/'
+paths = ['U5.fa', 'ADA.fa', 'FRAT1.fa', 'FXN.fa']
 lengths = []
 for n in paths:
-    lengths.append(seq_count(seq_read_fasta(n)))
+    lengths.append(seq_count(seq_read_fasta(folder + n)))
 
 data = dict(zip(paths, lengths))
 
 print('-----| Exercise 5 |------')
 for n in data:
-    print('Gene ' + n.split('/')[-1].replace('.fa', '') + ': ' + str(data[n]))
+    print('Gene ' + n.replace('.fa', '') + ': ' + str(data[n]))
