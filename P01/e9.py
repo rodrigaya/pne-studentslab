7fed83d0a493deda7d935001765faecdcfaa3088
rgaya <r.gaya.2023@alumnos.urjc.es>
2024-02-21 17:07:28 +0100

    S07

--- P01/e9.py
+++ P01/e9.py
@@ -2,10 +2,11 @@ from Seq1 import Seq
 
 print('-----| Practice 1, Exercise 9 |------')
 # -- Create a Null sequence
-s = Seq()
 
-seqs = [s.seq_read_fasta('../S04/Sequences/U5.fa')]
+seqs = ['../S04/Sequences/U5.fa']
 for n in seqs:
+    s = Seq()
+    s.seq_read_fasta(n)
     print(
-        f'Sequence {seqs.index(n) + 1}: (Length: {n.len()}) {n} \n  Bases: {n.count()} \n  Rev:  {n.seq_reverse()} \n  '
-        f'Comp: {n.seq_complement()}')
+        f'Sequence {seqs.index(n) + 1}: (Length: {s.len()}) {n} \n  Bases: {s.count()} \n  Rev:  {s.seq_reverse()} \n  '
+        f'Comp: {s.seq_complement()}')
