57db09ee7e4845a4b5a57ea19e75b531fd536027
rgaya <r.gaya.2023@alumnos.urjc.es>
2024-04-30 16:43:07 +0200

    whatever

--- P06/Seq2-server.py
+++ P06/Seq2-server.py
@@ -4,23 +4,31 @@ import socketserver
 from pathlib import Path
 
 
-def create_html(tool, title, body, color='white', link='/', link_text='Main page'):
-    result = '''<!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <title>''' + tool + '''</title>
-                </head>
-                <body style="background-color: ''' + color + ''';">
-                <h1>''' + title + '''</h1>
-                ''' + body + '''
-                <p></p>
-                <a href="''' + link + '''">''' + link_text + '''</a>
-                </body>
-                </html>
-                '''
-    return result
+def info(msg):
+    length = len(msg)
+    new = []
+    times = []
+    for n in msg:
+        if n not in new:
+            new.append(n)
+    for n in new:
+        time = 0
+        for ñ in msg:
+            if n == ñ:
+                time += 1
+        times.append(time)
+    corr = dict(zip(new, times))
+    result = \
+        'Total length: ' + str(length) + '\n'
+    for n in new:
+        string = ''
+        for a in range(len(new)):
+            string += new[a] + ': ' + corr[new[a]] + ' (' + str((int(corr[new[a]])/length) * 100) + '%)'
+    result += string
 
+
+
+    return result
 
 # Define the Server's port
 PORT = 8080
@@ -69,15 +77,20 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 body = base[base.find('\n'):].replace('\n', '')
                 contents = Path(folder + search + '.html').read_text().replace('{gene}', seq).replace('{body}', body)
             elif search == 'operation':
-                operation = self.requestline.split('?msg=')[1].split('%op=')[0]
-                msg = self.requestline.split('&op=')[0][-1].replace('=', '').upper()
+                operation = self.requestline.split('&op=')[1].split('HTTP/')[0].strip()
+                msg = self.requestline.split('&op=')[0].split('?msg=')[1].upper()
                 print(operation)
                 print(msg)
                 if operation == 'info':
-                    a = 'a'
-                #elif operation == 1:
-                #elif operation == 2:
-                contents = 'a'
+                    contents = Path(folder + search + '.html').read_text().replace('{seq}', msg).replace('{op}',
+                                                                                                         operation).replace(
+                        '{result}', str(info(msg)))
+                elif operation == 'comp':
+                    contents = 'a'
+                elif operation == 'rev':
+                    contents = 'a'
+                else:
+                    contents = Path('html/error.html').read_text()
             else:
                 contents = Path(folder + 'error.html').read_text()
         except FileNotFoundError:
