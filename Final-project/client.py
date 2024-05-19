8f2e285bfd34f493fa9b94203ad42c46af301e4a
rodrigaya <r.gaya.2023@alumnos.urjc.es>
2024-05-19 23:09:25 +0200

    Whatever2

---
+++ Final-project/client.py
@@ -0,0 +1,38 @@
+import http.client
+import http.server
+import json
+from termcolor import cprint
+
+
+def client(endpoint, params=None):
+    PORT = 8080
+    SERVER = 'localhost'
+
+    print(f"\nConnecting to server: {SERVER}:{PORT}\n")
+
+    # Connect with the server
+    conn = http.client.HTTPConnection(SERVER, PORT)
+
+    # -- Send the request message, using the GET method. We are
+    # -- requesting the main page (/)
+    try:
+        conn.request("GET", endpoint + params + 'json1')
+    except ConnectionRefusedError:
+        print("ERROR! Cannot connect to the Server")
+        exit()
+
+    # -- Read the response message from the server
+    r1 = conn.getresponse()
+
+    # -- Print the status line
+    print(f"Response received!: {r1.status} {r1.reason}\n")
+
+    # -- Read the response's body
+    data1 = r1.read().decode("utf-8")
+
+    # -- Create a variable with the data,
+    # -- form the JSON received
+    response = json.loads(data1)
+    return response
+
+
